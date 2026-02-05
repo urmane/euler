6a1361d002133c049aa0e4c5ada660e8c1772461
Jim Niemira <urmane@users.noreply.github.com>
2026-02-05 16:58:53 -0500

    add rust

--- p0/p0.rs
+++ p0/p0.rs
@@ -1,4 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let mut total: u128 = 0;
+    for i in 1..857000 {
+        if i * i % 2 != 0 {
+	    total += i * i;
+	}
+    }
+    println!("Total is {}", total);
 }
-
